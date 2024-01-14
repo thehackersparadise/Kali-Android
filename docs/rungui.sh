947e4ee20d3f57db684b23a669f342acdeeaec21
Techie <hackersparadise079@gmail.com>
2024-01-14 12:42:41 +0000

    Initial commit

---
+++ docs/rungui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#credits :- the hackers paradise
+clear
+apt-get install figlet -y
+clear
+figlet -f slant Kali-Android
+echo -e '\033[1;32m' "Instagram - @thehackersparadise_"
+echo -e '\033[1;32m' "Youtube   - @thehackersparadise_"
+echo -e '\033[1;32m' "Telegram  - @thehackersparadise5"
+echo -e '\033[1;32m' "-----------------------------------------"
+echo -e '\033[1;32m' "CODED BY THE HACKERS PARADISE"
+echo -e '\033[1;32m' "-----------------------------------------"
+# colors
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;34m'
+PINK='\033[1;35m'
+WHITE='\033[1;37m'
+#------------------------------------------------------------
+echo -e '\033[1;32m' "Remember,Password is 12345678"
+kex	
